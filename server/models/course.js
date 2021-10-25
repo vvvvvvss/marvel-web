5bfaae5bc60dd9dc5583f8753f9e136f6a6e65c7
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2021-10-25 22:51:08 +0530

    eod

--- server/models/course.js
+++ server/models/course.js
@@ -1,7 +1,7 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 
 const courseSchema = mongoose.Schema({
-
+    domainName : { type: String, enum:{values : [['AI-ML','IOT','D&P','EV-RE','CL-CY']]}}
 });
 
 export default mongoose.model('courses', courseSchema);
