893e4cbef3da5e5d128091de431a37f3e040d0ae
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2021-11-06 17:02:05 +0530

    user doc doc

--- server/models/user.js
+++ server/models/user.js
@@ -4,23 +4,35 @@ import slug from 'mongoose-slug-generator';
 mongoose.plugin(slug);
 
 const userSchema = mongoose.Schema({
+    // IDENTITY DATA ( does'nt change)
     slug :{type: String, slug : "name", unique : true},
     name: { type: String, required:  true },
     email: { type: String, required: true },
     profilePic : {type : String},
     regNo : { type : String },
     id: { type: String, required : true,unique:true },
+    joinedAt : {type : Date, default : new Date()},
+
+    // META DATA ( changes. auto )
+    currentRole : {type : String, enum : {values : ['STU','INS']}},
     currentStuCourse : {type : String},
     currentInsCourse : { type : [String]},
     currentLevel : {type : String, enum : {values : ['1','2','3']}},
-    currentRole : {type : String, enum : {values : ['STU','INS']}},
+    roleHistory : [{
+        role : {type : String, enum : {values : ['STU','INS']}},
+        startTime : {type : Date},endTime : {type : Date}
+    }],
     rights: { type : String, enum : {values : ['MOD','USER']}, default:'USER'},
-    joinedAt : {type : Date, default : new Date()},
     enrollmentStatus : { type : String, enum : {values : ['ACTIVE', 'INACTIVE', 'UNKNOWN','BANNED']}, default : 'UNKNOWN'},
+    
+    
+    // DIRECTLY EDITABLE
     bio : {type : String, maxLength : 200},
     gitHub : {type : String, maxLength:80},
     linkedIn : {type : String, maxLength:80},
     website : {type : String, maxLength:80},
+
+    // META DATA IF BANNED
     bannedAt : {type :String},
     bannedByName : {type :String},
     bannedById : {type : String}
