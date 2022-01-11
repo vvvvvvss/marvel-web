7df7137b8a7dcd7b9837f9171f64aa2223bc6001
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2022-01-11 19:10:07 +0530

    client api

--- client/src/API/index.js
+++ client/src/API/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL : 'http://localhost:3000/'});
+const API = axios.create({baseURL : 'https://marvel-web.azurewebsites.net/'});
 
 API.interceptors.request.use((req) => {
     if(sessionStorage.getItem('deez')){
