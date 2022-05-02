194c37e39b803b807b683ec95f089b0b946923f1
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2022-05-02 17:09:46 +0530

    bug fix

--- client/src/utils/hooks/useAuth.js
+++ client/src/utils/hooks/useAuth.js
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 export default ()=>{
     const navigate = useNavigate();
     const {data, isLoading, refetch:fetchAuth} = useQuery(['auth'], ()=>(auth()), {
-        enabled:!["undefined",""].includes(sessionStorage.getItem('deez')),
+        enabled:!["undefined","",null,undefined].includes(sessionStorage.getItem('deez')),
         onSuccess:(data)=>{
             if(data?.status==='404'){
                 navigate('/404');
