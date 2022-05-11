16b9add5dabcea1d8e03c9b04d071b8691cd373d
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2022-05-11 15:50:28 +0530

    version update

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ var apiKey = defaultClient.authentications['api-key'];
 apiKey.apiKey = process.env.SIB_API_KEY;
 
 app.get('/', (req,res)=> {
-  res.send('Welcome to UVCE Marvel REST API. v 2.01.')
+  res.send('Welcome to UVCE Marvel REST API. v 2.05.')
 });
 
 //starting points for routes
