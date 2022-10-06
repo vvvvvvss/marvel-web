6a714167c7b3e24a8e0f611e3012c6be5438e78c
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2022-10-07 01:45:04 +0530

    eh

--- workspace/libs/web-utils/src/lib/models/courseWork.ts
+++ workspace/libs/web-utils/src/lib/models/courseWork.ts
@@ -1,7 +1,9 @@
 import mongoose, { Schema } from 'mongoose';
+import slug from 'mongoose-slug-generator';
+mongoose.plugin(slug);
 
 //author entity
-const authorsSchema = new Schema(
+const personEntity = new Schema(
   {
     id: String,
     slug: String,
@@ -14,17 +16,14 @@ const authorsSchema = new Schema(
 //course workbook
 const courseWorkSchema = new Schema(
   {
-    authors: [authorsSchema],
-    level: { type: Number, required: true },
+    authors: [personEntity],
+    level: { type: Number, required: true, default: 1 },
     totalLevels: { type: Number, required: true },
     courseCode: { type: String, required: true },
     domain: { type: String, required: true },
-    slug: { type: String, slug: ['title', 'authorName'], unique: true },
+    slug: { type: String, slug: ['_id', 'courseCode'], unique: true },
 
-    // EDITABLE DATA
-    title: { type: String, maxLength: 120, required: true },
-    content: { type: String, required: true, maxLength: 15000 },
-    tags: { type: [String], default: [] },
+    reports: [{}],
 
     // META DATA
     reviewStatus: {
@@ -32,7 +31,6 @@ const courseWorkSchema = new Schema(
       enum: { values: ['PENDING', 'APPROVED', 'FLAGGED', 'FEATURED'] },
       default: 'PENDING',
     },
-    feedback: { type: String, maxLength: 500 },
     rankingScore: { type: Number, default: 1 },
   },
   { collection: 'works', timestamps: true }
@@ -40,9 +38,18 @@ const courseWorkSchema = new Schema(
 
 const projectWorkSchema = new Schema(
   {
-    windGust: String,
-    windDirection: String,
-    windSpeed: String,
+    authors: [personEntity],
+    name: String,
+    slug: { type: String, slug: ['name'], unique: true },
+    coordinators: [personEntity],
+
+    // META DATA
+    reviewStatus: {
+      type: String,
+      enum: { values: ['PENDING', 'APPROVED', 'FLAGGED', 'FEATURED'] },
+      default: 'PENDING',
+    },
+    rankingScore: { type: Number, default: 1 },
   },
   { collection: 'works', timestamps: true }
 );
