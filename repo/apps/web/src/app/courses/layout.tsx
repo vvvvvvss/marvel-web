d545b66ab0d5d668e529d6ab7a48a5ad69087872
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2023-03-22 20:58:37 +0530

    og in course list

--- repo/apps/web/src/app/courses/layout.tsx
+++ repo/apps/web/src/app/courses/layout.tsx
@@ -5,6 +5,22 @@ export const metadata: Metadata = {
   title: "List of all Courses. | UVCE MARVEL",
   description:
     "An exhaustive list of courses across five domains at MARVEL: Artificial Intelligence & Machine Learning, Internet of Things, Cloud Computing and Cybersecurity, Design & Prototyping, and Renewable Energy Tech.",
+  openGraph: {
+    type: "book",
+    title: "List of all Courses. | UVCE MARVEL",
+    description:
+      "An exhaustive list of courses across five domains at MARVEL: Artificial Intelligence & Machine Learning, Internet of Things, Cloud Computing and Cybersecurity, Design & Prototyping, and Renewable Energy Tech.",
+    images: [
+      {
+        url: "https://res.cloudinary.com/marvelweb/image/upload/v1678988482/Group_38_qrhqag.jpg",
+        secureUrl:
+          "https://res.cloudinary.com/marvelweb/image/upload/v1678988482/Group_38_qrhqag.jpg",
+        type: "image/jpeg",
+        width: 800,
+        height: 800,
+      },
+    ],
+  },
 };
 
 export default async function layout({ children, params }) {
