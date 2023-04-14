a986e4c6ee20d63a085f9d8730aa26571264590c
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2023-04-14 13:01:02 +0530

    moved metadate

--- repo/apps/web/src/app/events/page.tsx
+++ repo/apps/web/src/app/events/page.tsx
@@ -6,26 +6,6 @@ import { EventCard } from "../../components/Cards";
 import { useInfiniteQuery } from "react-query";
 import axios from "axios";
 
-export const metadata: Metadata = {
-  title: "Events. | UVCE MARVEL",
-  description: "Events, Workshops, Competitions and Talks at UVCE MARVEL",
-  openGraph: {
-    type: "website",
-    title: "Events at UVCE MARVEL",
-    description: "Events, Workshops, Competitions and Talks at UVCE MARVEL",
-    images: [
-      {
-        url: "https://res.cloudinary.com/marvelweb/image/upload/v1679946683/Frame_1_kjxodv.jpg",
-        secureUrl:
-          "https://res.cloudinary.com/marvelweb/image/upload/v1679946683/Frame_1_kjxodv.jpg",
-        type: "image/jpeg",
-        width: 800,
-        height: 800,
-      },
-    ],
-  },
-};
-
 export default function page({ params }) {
   const { data, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage } =
     useInfiniteQuery(
