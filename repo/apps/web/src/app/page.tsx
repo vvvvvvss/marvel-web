2bc4c3760e9882735701f86601af6666a75d508e
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2023-04-14 18:02:49 +0530

    tracs

--- repo/apps/web/src/app/page.tsx
+++ repo/apps/web/src/app/page.tsx
@@ -47,22 +47,22 @@ export const metadata: Metadata = {
 
 const tracks: TrackDescription[] = [
   {
-    title: "Project Track",
-    desc: "Project Track is for students of UVCE who have a project idea but not the resources. MARVEL will cater to their needs, by providing its facilities and resources. In project track, students submit an abstract of the project, tentative timeline of the project, team details, and budget estimate. Team MARVEL shall screen the applications. After screening and interview/ abstract presentation, teams shall be a part of MARVEL.",
-    link: "https://forms.gle/aiD3WWXUBngknHzX9",
-    buttonText: "Apply",
-  },
-  {
-    title: "Student Track",
-    desc: "Here, you will have the opportunity to dive deep into your areas of interest, learning from the ground up through exciting projects and activities. Whether you're interested in robotics, 3D printing, electronics, or any other area of engineering, our Makerspace is the perfect place to develop your skills and passions. Join a community of like-minded students and researchers, and take your engineering education to the next level. Applications will be open once every 6 months to join the student batch.",
+    title: "Student",
+    desc: "With Student Track, you'll be able to gain valuable experience in your areas of interest through a comprehensive, hands-on curriculum that utilizes Marvel's resources from the very beginning. The program is designed as a batch program and applications for enrollment will be accepted twice a year, giving students a unique opportunity to learn and grow with a cohort of like-minded individuals.",
     link: "/courses",
     buttonText: "Explore Courses",
   },
   {
-    title: "Competition Track",
-    desc: "MARVEL offers support to UVCE students who wish to participate in technical competitions accross domains through the Competition Track. Students can submit their competition details, including the required budget estimate, and MARVEL will screen and select the most promising entries. MARVEL will then provide necessary resources such as financial assistance, mentorship, and guidance to ensure the success of the team in the competition.",
+    title: "Project",
+    desc: "Project Track is for UVCE students with project ideas but limited resources. Marvel provides facilities and resources to support these projects. Interested students can submit an abstract, timeline, team details, and budget estimate for consideration. Marvel's team screens applications and selects candidates for an interview or abstract presentation. Selected teams work closely with Marvel to turn their ideas into reality.",
+    link: "https://forms.gle/aiD3WWXUBngknHzX9",
+    buttonText: "Apply with an Idea",
+  },
+  {
+    title: "Competition",
+    desc: "MARVEL's Competition Track supports UVCE students who want to participate in technical competitions across domains. Students can submit competition details and budget estimates, and MARVEL will screen and select promising entries. MARVEL provides resources such as financial assistance, mentorship, and guidance to ensure team success.",
     link: "https://forms.gle/Ds6EoHK7iYt6Ki6NA",
-    buttonText: "Apply",
+    buttonText: "Apply with an Idea",
   },
 ];
 
@@ -106,31 +106,33 @@ export default function page() {
         <div className="p-5">
           <h1
             id={"tracks"}
-            className="capitalize tracking-widest text-2xl text-p-0 dark:text-p-5 border-y border-p-5 p-5"
+            className="capitalize tracking-widest text-p-0 dark:text-p-5 border-y border-p-5 dark:border-p-3 p-5"
           >
             TRACKS
           </h1>
-          <div className="flex gap-5 flex-wrap mt-5">
+          <div className="flex flex-col gap-10 py-10">
             {tracks?.map((t, i) => (
-              <Paper
+              <div
+                className="p-5 flex flex-col md:flex-row w-full justify-between gap-5"
                 key={i}
-                border
-                shadow
-                className="bg-p-10 dark:bg-p-1 rounded-lg p-5 flex flex-col gap-3 flex-1 max-h-fit justify-between min-w-[300px]"
               >
-                <h2 className="text-2xl">{t?.title}</h2>
-                <hr className="border-p-5" />
-                <p className="text-base text-p-0 dark:text-p-9">{t?.desc}</p>
-                <Link
-                  href={t?.link}
-                  target={t?.link?.startsWith("http") ? "_blank" : "_self"}
-                  rel={t?.link?.startsWith("http") ? "noopener noreferrer" : ""}
-                >
-                  <Button variant="outlined" className="w-full">
-                    {t?.buttonText}
-                  </Button>
-                </Link>
-              </Paper>
+                <h2 className="text-5xl flex-wrap leading-snug">
+                  {t?.title}
+                  <span className="text-p-5"> Track</span>
+                </h2>
+                <div className="max-w-sm flex flex-col gap-10">
+                  <p className="text-p-0 dark:text-p-9">{t?.desc}</p>
+                  <Link
+                    href={t?.link}
+                    target={t?.link?.startsWith("http") ? "_blank" : "_self"}
+                    rel={
+                      t?.link?.startsWith("http") ? "noopener noreferrer" : ""
+                    }
+                  >
+                    <Button variant="outlined">{t?.buttonText}</Button>
+                  </Link>
+                </div>
+              </div>
             ))}
           </div>
         </div>
