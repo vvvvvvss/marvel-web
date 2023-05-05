016ac7845e684c4d563651725c5b2734c5eca536
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2023-05-06 03:00:22 +0530

    focus fix

--- repo/packages/ui/src/Inputs/TextField.tsx
+++ repo/packages/ui/src/Inputs/TextField.tsx
@@ -51,7 +51,7 @@ export const TextField = ({
             "py-2 px-3 my-1 rounded-md dark:bg-p-2 bg-p-9 w-full",
             "transition ease-out",
             "hover:-translate-y-[1.5px] active:-translate-y-[1.5px] focus:-translate-y-[1.5px]",
-            "focus:bg-p-10 focus:dark:bg-p-1",
+            "focus:bg-p-10 focus:dark:bg-p-1 focus:outline-none",
             "border-[1.5px] dark:border border-p-4 dark:border-p-5",
             "placeholder:text-p-4 dark:placeholder:text-p-6",
             "disabled:opacity-50 disabled:cursor-not-allowed disabled:pointer-events-none"
