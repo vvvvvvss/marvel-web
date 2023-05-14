b68e6d07ffe965a00009adb6805e7b9f38241e40
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2023-05-14 13:35:13 +0530

    clean up

--- client/src/utils/functions/colorDecide.js
+++
@@ -1,5 +0,0 @@
-export default (status) => {
-    if(status==='PENDING') return 'warning';
-    else if (status==='FLAGGED') return 'error';
-    else if (status==='APPROVED' || 'FEATURED') return 'success';
-}
--- server/routes/utils.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import {authController} from '../controllers/auth.js';
-
-const router = express.Router();
-
-router.post('/auth', authController);
-
-export default router;
