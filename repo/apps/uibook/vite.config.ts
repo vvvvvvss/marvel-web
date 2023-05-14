4c5d8af7c885bb21da5ad51d0e5e843139a3d2bb
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2023-05-14 14:50:20 +0530

    rollup plugin

--- repo/apps/uibook/vite.config.ts
+++ repo/apps/uibook/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import postcss from "rollup-plugin-postcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    postcss({
+      modules: true,
+      minimize: true,
+    }),
+  ],
+});
